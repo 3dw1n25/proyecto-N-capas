8f561c757c18e9ce86e3ed56b4c289442095d030
3dw1n25 <3dw1n2000@gmail.com>
2021-06-23 17:22:01 -0600

    update base de datos

--- Proyecto_Ncapas.sql
+++ Proyecto_Ncapas.sql
@@ -1,35 +1,35 @@
 CREATE TABLE libros (
-id integer primary key identity,
-Numeroejemplar integer,
-ISBN varchar(100),
-Titulo varchar(25),
-Autor varchar(150),
-Editorial varchar(25),
-AñoEdicion varchar(10),
-NumeroEdicion varchar(15),
-Pais varchar(20),
-idioma varchar(15),
-Materia varchar(15),
-NumeroPaginas integer,
-Ubicacion varchar(100),
-Descripcion varchar(200)
+	id integer primary key identity,
+	Numeroejemplar integer,
+	ISBN varchar(100),
+	Titulo varchar(25),
+	Autor varchar(150),
+	Editorial varchar(25),
+	AñoEdicion varchar(10),
+	NumeroEdicion varchar(15),
+	Pais varchar(20),
+	idioma varchar(15),
+	Materia varchar(15),
+	NumeroPaginas integer,
+	Ubicacion varchar(100),
+	Descripcion varchar(200)
 );
 
 CREATE TABLE video(
-codigo integer primary key identity,
-Titulo varchar(20),
-Director varchar(40),
-Productora varchar (25),
-tipo varchar(10),
-Año varchar(10),
-Duracion varchar(10),
-Pais varchar(20),
-idioma varchar(15),
-subtitulos varchar (15),
-Clasificacion varchar(10),
-Genero varchar(10),
-sinopsis varchar (200),
-ubicacion varchar (100),
+	codigo integer primary key identity,
+	Titulo varchar(20),
+	Director varchar(40),
+	Productora varchar (25),
+	tipo varchar(10),
+	Año varchar(10),
+	Duracion varchar(10),
+	Pais varchar(20),
+	idioma varchar(15),
+	subtitulos varchar (15),
+	Clasificacion varchar(10),
+	Genero varchar(10),
+	sinopsis varchar (200),
+	ubicacion varchar (100),
 );
 
 
@@ -42,7 +42,8 @@ create table rol (
 
 
 CREATE TABLE usuario (
-nombre varchar(40) primary key,
+id integer primary key identity,
+nombre varchar(40),
 contraseña varchar(40),
 idrol integer not null,
 FOREIGN KEY (idrol) REFERENCES rol(idrol)
@@ -51,11 +52,13 @@ FOREIGN KEY (idrol) REFERENCES rol(idrol)
 
 create table prestamo (
 	idprestamo integer primary key identity,
-	idusuario varchar(40) not null,
-	idarticulo integer not null,
+	idusuario integer not null,
+	idlibro integer not null,
+	idvideo integer not null,
 	fecha datetime not null,
 	fechadev datetime not null,
-	estado varchar(20) not null,
-	FOREIGN KEY (idusuario) REFERENCES usuario(nombre),
-	FOREIGN KEY (idarticulo) REFERENCES libros(id)
+	estado BIT NULL DEFAULT 1,
+	FOREIGN KEY (idusuario) REFERENCES usuario(id),
+	FOREIGN KEY (idvideo) REFERENCES video(codigo),
+	FOREIGN KEY (idlibro) REFERENCES libros(id)
 );
