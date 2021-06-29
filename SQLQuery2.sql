cd80f8116760b71d0a47d1fc8e3c21cef94ca35e
Francisco Rodriguez <00060618@uca.edu.sv>
2021-06-29 16:33:35 -0600

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -46,10 +46,56 @@ go
 create proc bucar_video
 @valor varchar(50)
 as
-select codigo as Código, Titulo as Tituo, Director, Productora
+select codigo as Código, Titulo as Tituo, Director
 from video
-where Titulo like '%' + @valor + '%' or Director like '%' +@valor + '%' or Productora like '%' +@valor + '%'
+where Titulo like '%' + @valor + '%' or Director like '%' +@valor + '%' 
 order by codigo desc
 go
 
 
+--insertar
+create proc libro_insertar
+@Numeroej integer,
+@ISBN varchar(100),
+@Titulo varchar(25),
+@Autor varchar(150),
+@Editorial varchar(25),
+@AñoEdicion varchar(10),
+@NumeroEdicion varchar(15),
+@Pais varchar(20),
+@idioma varchar(15),
+@Materia varchar(15),
+@NumeroPaginas integer,
+@Ubicacion varchar(100),
+@Descripcion varchar(200)
+as
+insert into libros (Numeroejemplar,ISBN,Titulo,Autor,Editorial,AñoEdicion,NumeroEdicion,Pais,idioma,Materia,NumeroPaginas,Ubicacion,Descripcion)
+values (@Numeroej,@ISBN,@Titulo,@Autor,@Editorial,@AñoEdicion,@NumeroEdicion,@Pais,@idioma,@Materia,@NumeroPaginas,@Ubicacion,@Descripcion)
+go
+
+create proc video_insertar
+	@Titulo varchar(20),
+	@Director varchar(40),
+	@Productora varchar (25),
+	@tipo varchar(10),
+	@Año varchar(10),
+	@Duracion varchar(10),
+	@Pais varchar(20),
+	@idioma varchar(15),
+	@subtitulos varchar (15),
+	@Clasificacion varchar(10),
+	@Genero varchar(10),
+	@sinopsis varchar (200),
+	@ubicacion varchar (100)
+	as
+	insert into video (Titulo,Director,Productora,tipo,Año,Duracion,Pais,idioma,subtitulos,Clasificacion,Genero,sinopsis,ubicacion)
+	values (@Titulo,@Director,@Productora,@tipo,@Año,@Duracion,@Pais,@idioma,@subtitulos,@Clasificacion,@Genero,@sinopsis,@ubicacion)
+	go
+
+create procedure usr_insertar
+@nombre varchar(40),
+@contraseña varchar(40),
+@apellido varchar(40)
+as
+insert into usuario (nombre,apellido,contraseña)
+values (@nombre,@apellido,@contraseña)
