8fd3b92c4ab74241e52c7d7c4fd255d66054d8e2
3dw1n25 <3dw1n2000@gmail.com>
2021-06-29 16:41:04 -0600

    conexion

---
+++ Biblioteca/Sistema.Datos/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sistema.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "Proyecto_Ncapas";
+            this.Servidor = "190.53.31.203";
+            this.Usuario = "sa";
+            this.Clave = "8501";
+            this.Seguridad = true; //Seguridad de Windows
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";
+                if (this.Seguridad) //Seguridad de Windows
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+                else //Seguridad de SQL Server
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id=" + this.Usuario + "; Password=" + this.Clave;
+                }
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+
+    }
+}
